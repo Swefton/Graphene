59c8c85d9a06c3084c9dc1520adf99f907a8af96
Swefton <amrit.madhav.srivastava@gmail.com>
2025-02-21 01:37:07 -0500

    some kinda listeners for everything relevant, need to design the graph structure now

--- public/background.js
+++ public/background.js
@@ -1,35 +1,22 @@
-const tabGraph = {}; // Stores tab relationships
+const newTabs = new Set();
 
 chrome.tabs.onCreated.addListener((tab) => {
-  if (tab.openerTabId) {
-    // If the tab was opened from another tab, record the relationship
-    chrome.tabs.get(tab.openerTabId, (openerTab) => {
-      if (openerTab && openerTab.url) {
-        tabGraph[tab.id] = {
-          from: openerTab.url, // Source node
-          to: tab.pendingUrl || tab.url || "New Tab", // Destination node
-          createdAt: Date.now(), // Timestamp for tracking
-        };
-        console.log("New tab relationship:", tabGraph[tab.id]);
-      }
-    });
-  }
+  console.log("New tab opened:", tab);
+  newTabs.add(tab.id);
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url) {
-    if (tabId in tabGraph) {
-      // Update the destination URL if it changed
-      tabGraph[tabId].to = changeInfo.url;
-      console.log("Updated tab navigation:", tabGraph[tabId]);
+    if (newTabs.has(tabId)) {
+      newTabs.delete(tabId); 
     } else {
-      // If the tab was already open, just track navigation
-      tabGraph[tabId] = {
-        from: "Unknown", // No opener, treat it as a new session
-        to: changeInfo.url,
-        createdAt: Date.now(),
-      };
-      console.log("Navigation without opener:", tabGraph[tabId]);
+      console.log("User navigated to:", changeInfo.url);
     }
   }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    console.log("Current Tab: ", activeInfo.tabId)
+  });
 });
