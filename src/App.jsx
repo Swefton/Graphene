fd7c222f4bf346f5a194510dca0cabc45a75cc90
Swefton <amrit.madhav.srivastava@gmail.com>
2025-03-09 21:13:49 -0400

    Added button to new page
    added a button that opens a new page, the design graph, style, etc. still needs to be added to the new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import ForceGraph2D from 'react-force-graph-2d';
+import ForceGraph2D from "react-force-graph-2d";
 
 const App = () => {
   const [graphData, setGraphData] = useState({ nodes: [], links: [] });
@@ -13,33 +13,67 @@ const App = () => {
         }
       });
     }
-  }, []); 
+  }, []);
+
+  const openGraphInNewTab = () => {
+    window.open("fullpage.html", "_blank");
+  };
 
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <h1>Browsing Graph</h1>
-      <ForceGraph2D
-        graphData={graphData}
-        height={600}
-        width={400}
-        backgroundColor="#1f1d2e"
-        ref={fgRef}
-        nodeAutoColorBy="id"
-        linkColor={() => "pink"}
-        linkDirectionalArrowColor={() => "pink"}
-        linkDirectionalArrowLength={5}
-        linkDirectionalArrowRelPos={1}
-        nodeLabel={(node) => node.name}
-        nodeCanvasObject={(node, ctx) => {
-          const label = node.name;
-          const fontSize = 8;
-          ctx.font = `${fontSize}px Sans-Serif`;
-          ctx.fillStyle = "white";
-          ctx.textAlign = "center";
-          ctx.textBaseline = "middle";
-          ctx.fillText(label, node.x, node.y);
+    <div
+      style={{
+        width: "500px",
+        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#1f1d2e",
+        color: "white",
+        padding: "10px",
+        boxSizing: "border-box",
+      }}
+    >
+      <h1 style={{ margin: "5px 0" }}>Browsing Graph</h1>
+      <div style={{ flex: 1, width: "100%" }}>
+        <ForceGraph2D
+          graphData={graphData}
+          height={400} // Adjusted height to fit in the box
+          width={480} // Slightly smaller than container width
+          backgroundColor="#1f1d2e"
+          ref={fgRef}
+          nodeAutoColorBy="id"
+          linkColor={() => "pink"}
+          linkDirectionalArrowColor={() => "pink"}
+          linkDirectionalArrowLength={5}
+          linkDirectionalArrowRelPos={1}
+          nodeLabel={(node) => node.name}
+          nodeCanvasObject={(node, ctx) => {
+            const label = node.name;
+            const fontSize = 8;
+            ctx.font = `${fontSize}px Sans-Serif`;
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText(label, node.x, node.y);
+          }}
+        />
+      </div>
+      <button
+        style={{
+          display: "block",
+          marginTop: "10px",
+          padding: "8px 15px",
+          backgroundColor: "#ff4081",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
         }}
-      />
+        onClick={openGraphInNewTab}
+      >
+        Open Graph
+      </button>
     </div>
   );
 };
