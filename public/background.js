f7d261202bb4e71d0b8cbe19eaadabf53da47f04
Amrit <amrit.madhav.srivastava@gmail.com>
2025-06-01 01:45:02 -0400

    window tracking added
    window tracking has been added, next we need graphs per each window

--- public/background.js
+++ public/background.js
@@ -4,16 +4,60 @@
 // open tabIds in the window
 
 class Window {
-  constructor(graph, curr, openTabs) {
-    this.graph = graph
-    this.curr = curr
-    this.openTabs = openTabs
+  constructor() {
+    this.graph = {nodes: [], links: []}
+    this.nodeSet = new Set()
+    this.openTabs = {}
+    this.curr = null 
   }
+}
+
+// Node Class 
+class Node {
+  constructor(url, name, tabId) {
+    this.id = `${url}_${tabId}`; 
+    this.tabId = tabId;
+    this.url = url;
+    this.name = name;
+  }
+}
+
+function addLink(window, node) {
+  nodeSet = window.nodeSet;
+  graph = window.graph;
+  openTabs = window.openTabs;
+  curr = window.curr;
+
+  if (!nodeSet.has(node)){
+    graph.nodes.push(node)
+    nodeSet.add(node)
+  }
+
+  sourceNodeId = openTabs[curr.tabId]
+  if (sourceNodeId !== node.id && 
+    !graph.links.find({source: node.id, target: sourceNodeId})
+  ) {
+    graph.links.push({
+      source: sourceNodeId,
+      target: node.id
+    });
+  }
+
+  openTabs[node.tabId] = node.id;
 }
 
 chrome.windows.onFocusChanged.addListener((windowId) => {
   console.log("Window Focus Changed")
   console.log(windowId)
+  chrome.storage.local.get(["windows"], (data) => {
+    let windowsData = data.windows || {};
+    if (windowId !== -1 && !(windowId in windowsData)) {
+      windowsData[windowId] = new Window()
+      chrome.storage.local.set({windows: windowsData})
+      console.log(`Saved window ${windowId}:`);
+      console.log(windowsData[windowId])
+    }
+  })
 })
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
@@ -27,3 +71,14 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
   console.log("Tab Activated")
   console.log(activeInfo)
 })
+
+// Listeners for calls from front end
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "deleteGraphData") {
+    chrome.storage.local.clear(() => {
+      console.log("Local Storage Cleared.")
+    })
+    sendResponse({ success: true });
+    return true; 
+  }
+});
